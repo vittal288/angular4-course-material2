cb02a798dce6b4deac65faf88a76b172085b37cf
Vittal <vittal.kamkar2@mindtree.com>
2017-11-11 18:43:52 +0530

    first commit

---
+++ bs-config.js
@@ -0,0 +1,11 @@
+module.exports={
+    server:{
+        baseDir:"./dist",
+        middleware:{
+            1:require('connect-history-api-fallback')({
+                index:'/index.html',
+                verbose:true
+            }) //enable routing with hash 
+        }
+    }
+}
